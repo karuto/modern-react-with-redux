6465b1d6d8af19546fb9996b781c2ca0dff2031f
Vincent Zhang <vzhang@opentable.com>
2017-10-10 14:04:11 +0100

    Added list item, converted App from functional to class-based component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import VideoListItem from './video_list_item';
 
 // create a video list component that will take a list of video list items.
 // for each item, render it with a unique key so that React doesn't have warnings.
 
 
-const VideoList = () => {
+const VideoList = (props) => {
+  const videoListItems = props.videos.map((item) => {
+      return <VideoListItem data={item}/>;
+  });
   return (
-    <ul>Heyo</ul>
+    <div>
+      {videoListItems}
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -6,16 +6,33 @@ import YTSearch from "youtube-api-search";
 
 const API_KEY = 'AIzaSyBbzrBTewQDY8LkkLKkyqrGPM6ci0kRunU';
 const videos = YTSearch({key: API_KEY, term: 'yoyo'}, (videos) => {
-  console.log(videos);
+  return videos;
 });
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-      <VideoList />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    };
+
+    YTSearch({key: API_KEY, term: 'yoyo'}, (videos) => {
+      this.setState({
+        videos: videos
+      });
+    });
+
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
